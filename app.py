3cb78be9a191f8c69eb79374853c47d77cc5af7b
Whit Jack <whit@whitjack.me>
2016-12-08 08:59:29 -0800

    Fix space

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def slack():
         return 'I need some text.', 200
 
     try:
-        out = partyryan.convert_str_to_emoji(text, space=(EN_SPACE * 4))
+        out = partyryan.convert_str_to_emoji(text, space=(EN_SPACE * 2))
         # because slack trims the beginning of messages now,
         # and unicode spaces don't help,
         # replace the first character with a period.
